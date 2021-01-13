77872d36debbb21b1f8352747bda664104656103
Ishaan-10 <b.ishaan10@gmail.com>
2021-01-13 16:42:57 +0530

    Added JS for categories

---
+++ templates/FAQ/static/js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".targetDiv").hide();
+  $("#div1").show();
+  $(".Single1").click(function () {
+    $(".targetDiv").hide();
+    $("#div1").show();
+  });
+  $(".Single2").click(function () {
+    $(".targetDiv").hide();
+    $("#div2").show();
+  });
+  $(".Single3").click(function () {
+    $(".targetDiv").hide();
+    $("#div3").show();
+  });
+  $(".Single4").click(function () {
+    $(".targetDiv").hide();
+    $("#div4").show();
+  });
+});
