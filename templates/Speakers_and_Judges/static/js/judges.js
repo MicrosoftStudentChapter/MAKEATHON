3184adb6dcb3727b8557594fd3c9ff527b1bde7d
Suprit Behera <supritb12@gmail.com>
2021-01-18 00:21:27 +0530

    Add skeleton speakers.js and judges.js file

---
+++ templates/Speakers_and_Judges/static/js/judges.js
@@ -0,0 +1,16 @@
+let judgeTotalNumber;
+
+let judgeNames = [];
+let judgeImageSrc = [];
+
+let judgeInsta = [];
+let judgeLinkedIn = [];
+let judgeGitHub = [];
+
+let judgeInfoHeading2 = [];
+let judgeInfoDate = [];
+let judgeInfoPlatformAndTime = [];
+let judgeInfoSummary = [];
+
+
+
---
+++ templates/Speakers_and_Judges/static/js/speakers.js
@@ -0,0 +1,16 @@
+let speakerTotalNumber;
+
+let speakerNames = [];
+let speakerImageSrc = [];
+
+let speakerInsta = [];
+let speakerLinkedIn = [];
+let speakerGitHub = [];
+
+let speakerInfoHeading2 = [];
+let speakerInfoDate = [];
+let speakerInfoPlatformAndTime = [];
+let speakerInfoSummary = [];
+
+
+
