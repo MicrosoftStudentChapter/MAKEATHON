ccbc141885bef66d1529c9d563c0a4363e4845d4
Suprit Behera <supritb12@gmail.com>
2021-01-18 03:03:30 +0530

    Setup basic speaker module pattern template

--- templates/Speakers_and_Judges/static/js/speakers.js
+++ templates/Speakers_and_Judges/static/js/speakers.js
@@ -1,34 +1,79 @@
-// Total number of speakers
-let speakerTotalNumber;
+// Speakers module pattern
+let speakers = (function(){
+  // Total number of speakers
+  let speakerTotalNumber;
 
-let speakerNames = [];
-let speakerImageSrc = [];
+  let speakerNames = [];
+  let speakerImageSrc = [];
 
-let speakerInsta = [];
-let speakerLinkedIn = [];
-let speakerGitHub = [];
+  let speakerInsta = [];
+  let speakerLinkedIn = [];
+  let speakerGitHub = [];
 
-// Speaker attributes for their info page
-let speakerInfoHeading2 = [];
-let speakerInfoDate = [];
-let speakerInfoPlatformAndTime = [];
-let speakerInfoSummary = [];
+  // Speaker attributes for their info page
+  let speakerInfoHeading2 = [];
+  let speakerInfoDate = [];
+  let speakerInfoPlatformAndTime = [];
+  let speakerInfoSummary = [];
 
-// Factory function which returns a speaker object
-let speakerFactory = function(name, imgSrc, insta, linkedin, github, 
-  infoHeading2, infoDate, infoPlatformAndTime, infoSummary){
-    return{
-      name,
-      imgSrc,
-      insta,
-      linkedin,
-      github,
-      infoHeading2,
-      infoDate,
-      infoPlatformAndTime,
-      infoSummary,  
-    }  
-}
+  // Array that stores all the speaker objects
+  let speakerObjects = [];
+
+  _init();
+  function _init(){
+    addSpeakersToSpeakerObjects();
+  }
+
+  // Factory function which returns a speaker object
+  let createSpeakerObject = function(name, imgSrc, insta, linkedin, github, 
+    infoHeading2, infoDate, infoPlatformAndTime, infoSummary){
+      return{
+        name,
+        imgSrc,
+        insta,
+        linkedin,
+        github,
+        infoHeading2,
+        infoDate,
+        infoPlatformAndTime,
+        infoSummary,  
+      }  
+  }
+
+  function addSpeakersToSpeakerObjects(){
+    for(let i = 0; i < speakerTotalNumber; i++){
+      speakerObjects.push(
+        createSpeakerObject(
+          speakerNames[i],
+          speakerImageSrc[i],
+          speakerInsta[i],
+          speakerLinkedIn[i],
+          speakerGitHub[i],
+          speakerInfoHeading2[i],
+          speakerInfoDate[i],
+          speakerInfoPlatformAndTime[i],
+          speakerInfoSummary[i]
+        )
+      ) 
+    }
+  }
+
+  function getSpeakerTotalNumber(){
+    return speakerTotalNumber;
+  }
+
+  function getSpeakerObjects(){
+    return speakerObjects;
+  }
+
+  return{
+    getSpeakerObjects,
+    getSpeakerTotalNumber
+  }
+
+})();
+
+
 
 
 
