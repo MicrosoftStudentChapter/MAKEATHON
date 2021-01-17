9c377d41de75d13f35304801a2c4bcd90471977a
Suprit Behera <supritb12@gmail.com>
2021-01-18 04:17:34 +0530

    Dynamically create speaker cards and add to DOM, except social links

--- templates/Speakers_and_Judges/static/js/index.js
+++ templates/Speakers_and_Judges/static/js/index.js
@@ -1,5 +1,7 @@
 //import { speakers } from './speakers.js'
 
+import { speakers } from "./speakers";
+
 function showSpeakersAndHideJudges() {
   document.querySelector("#judges").style.display = "none";
   document.querySelector("#speakers").style.display = "grid";
@@ -20,7 +22,37 @@ function showJudgeAndHideInfo() {
   document.querySelector("#info").style.display = "block";
 }
 
-let imgperson1 = document.getElementById('imgperson1');
-imgperson1.addEventListener("click", showInfoAndHideCards);
+// let imgperson1 = document.getElementById('imgperson1');
+// imgperson1.addEventListener("click", showInfoAndHideCards);
+
+function createSpeakerCardsInDOM(){
+  let speakerWrapper = document.getElementById("speakers");
+
+  let pageHeader = document.createElement("h1");
+  pageHeader.classList.add("header");
+  pageHeader.textContent = "Speakers";
+  speakerWrapper.append(pageHeader);
+
+  // for(let i = 0; i < speakers.getSpeakerTotalNumber; i++){
+  //   let personContainer = document.createElement("div");
+  //   personContainer.classList.add = "person";
+
+  //   let personImage
+  // }
+
+  for(person in speakers.getSpeakerObjects()){
+    let personContainer = document.createElement("div");
+    personContainer.classList.add = "person";
+    
+    let personImg = document.createElement("img");
+    personImg.src = `${person.imgSrc}`;
+    personImg.addEventListener("click", showInfoAndHideCards);
+    personContainer.append(personImg);
+
+    let personNameHeader = document.createElement("h5");
+    personNameHeader.textContent = `${person.name}`;
+    personContainer.append(personNameHeader);
+  }
+}
 
 //console.log(speakers.getSpeakerTotalNumber());
