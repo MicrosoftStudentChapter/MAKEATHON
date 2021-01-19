8dbf8f712583496edbb3046b05eb82ffc284cd4d
Suprit Behera <supritb12@gmail.com>
2021-01-19 21:47:08 +0530

    Add documentation to judges.js

--- templates/Speakers_and_Judges/static/js/judges.js
+++ templates/Speakers_and_Judges/static/js/judges.js
@@ -1,8 +1,17 @@
-//Judges module pattern
+// judges module pattern is exported to be used in index.js
+// Only the three parameters in the return of judges, that is 
+// init, getJudgeObjects() and getJudgeTotalNumber() is accessible 
+// wherever it would be imported
+
 export let judges = (function(){
   // Total number of judges
+  // In case of adding of addition or removal of judges
+  // the value stored in it needs to be updated 
   let judgeTotalNumber = 6;
 
+  // Array having the names of all the judges
+  // First element of the array corresponds to the 
+  // name of the first judge and so on
   let judgeNames = [
     'RICHARD STALLMAN',
     'LINUS TORVALDS',
@@ -11,6 +20,8 @@ export let judges = (function(){
     'LINUS TORVALDS',
     'EDWARD SNOWDEN',
   ];
+
+  // Array having the path of the image of each judge
   let judgeImageSrc = [
     './static/images/img2.png',
     './static/images/img3.png',
@@ -19,7 +30,8 @@ export let judges = (function(){
     './static/images/img3.png',
     './static/images/pichai.png',
   ];
-
+    
+  // Array having the instagram id of each judge
   let judgeInsta = [
     'https://www.instagram.com/mlsc_tiet/?hl=en',
     'https://www.instagram.com/microsoft/?hl=en',
@@ -28,6 +40,8 @@ export let judges = (function(){
     'https://www.instagram.com/mlsc_tiet/?hl=en',
     'https://www.instagram.com/microsoft/?hl=en',
   ];
+
+  // Array having the linkedin url of each judge
   let judgeLinkedIn = [
     'https://www.linkedin.com/company/microsoft',
     'https://in.linkedin.com/company/microsoft-learn-student-chapter',
@@ -36,6 +50,8 @@ export let judges = (function(){
     'https://www.linkedin.com/company/microsoft',
     'https://in.linkedin.com/company/microsoft-learn-student-chapter',
   ];
+
+  // Array having the GitHub url of each judge
   let judgeGitHub = [
     'https://github.com/microsoft',
     'https://github.com/MicrosoftStudentChapter',
@@ -45,7 +61,9 @@ export let judges = (function(){
     'https://github.com/MicrosoftStudentChapter',
   ];
 
-  // judge attributes for their info page
+  /* -----------------Info Page Attributes-----------------*/
+
+  // Array containing secondary heading below the name to be displayed in the info page
   let judgeInfoHeading2 = [
     'DAY 1',
     'DAY 2',
@@ -54,6 +72,9 @@ export let judges = (function(){
     'DAY 1',
     'DAY 2',
   ];
+  
+  // Array containing the date, or whichever tertiary heading text lies 
+  // just below the secondary heading
   let judgeInfoDate = [
     'DECEMBER 69, 2069',
     'APRIL 20, 2042',
@@ -62,6 +83,9 @@ export let judges = (function(){
     'DECEMBER 69, 2069',
     'APRIL 20, 2042',
   ];
+
+  // Array containing the platform and the time
+  // or whichever text lies just below the judgeInfoDate[] corresponding value
   let judgeInfoPlatformAndTime = [
     'JITSI MEET, 4:20 PM',
     'YOUTUBE LIVE, 6:30 PM',
@@ -70,6 +94,8 @@ export let judges = (function(){
     'YOUTUBE LIVE, 6:30 PM',
     'MASTODON 4:20 PM'
   ];
+  
+  // Array containing the paragraph to be displayed on the info page
   let judgeInfoSummary = [
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.',
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.',
@@ -86,11 +112,6 @@ export let judges = (function(){
   function init(){
     addJudgesToJudgeObjects();
   }
-
-  // const imgFilePath = "./static/images";
-
-  // TODO : Add function that maps img file names to file path by appending
-  // file path in front of every image name
 
   // Factory function which returns a judge object
   let createJudgeObject = function(id, name, imgSrc, insta, linkedin, github, 
@@ -109,6 +130,9 @@ export let judges = (function(){
       }  
   }
 
+  // It iterates through all the arrays containing the attributes,
+  // passing the ith index of each to the factory function, and then
+  // pushes the created object to the judgeObjects[] array
   function addJudgesToJudgeObjects(){
     for(let i = 0; i < judgeTotalNumber; i++){
       judgeObjects.push(
