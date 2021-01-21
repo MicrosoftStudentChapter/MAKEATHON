4993eefd611e864d59b2cfee9ef650e44df0d70b
MJ-06 <mitesh.jindal08@gmail.com>
2021-01-21 14:21:08 +0530

    ran prettier on every file changed

--- templates/Tracks/static/js/index.js
+++ templates/Tracks/static/js/index.js
@@ -5,32 +5,32 @@ let alpha = 1;
 let flag = false;
 
 let configs = {
-  length: getViewHeight(12.5),
-  color: "rgb(255, 0, 212)",
-  lineWidth: 10,
-  shadowColor: "white",
-  shadowBlur: 10,
+    length: getViewHeight(12.5),
+    color: "rgb(255, 0, 212)",
+    lineWidth: 10,
+    shadowColor: "white",
+    shadowBlur: 10,
 };
 
 let startPosition = {
-  x: 0,
-  y: getViewHeight(12.5),
+    x: 0,
+    y: getViewHeight(12.5),
 };
 
 let heightOfTxtBox = 100;
 let updatedPosition;
 
 if (window.innerHeight < 700) {
-  heightOfTxtBox = 80;
+    heightOfTxtBox = 80;
 } else if (window.innerHeight < 600) {
-  heightOfTxtBox = 60;
+    heightOfTxtBox = 60;
 } else if (window.innerHeight < 500) {
-  heightOfTxtBox = 45;
+    heightOfTxtBox = 45;
 }
 
 document.querySelectorAll(".text").forEach((element) => {
-  element.style.height = heightOfTxtBox + "px";
-  element.style.width = heightOfTxtBox * 2 + "px";
+    element.style.height = heightOfTxtBox + "px";
+    element.style.width = heightOfTxtBox * 2 + "px";
 });
 
 updateResolution();
@@ -46,100 +46,100 @@ let animationInterval = setInterval(animateMyDrawing, 62.5);
 /*--------------------------- Functions ---------------------------*/
 
 function drawShape(alpha) {
-  c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  c.globalAlpha = alpha;
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    c.globalAlpha = alpha;
 
-  updatedPosition = JSON.parse(JSON.stringify(startPosition));
+    updatedPosition = JSON.parse(JSON.stringify(startPosition));
 
-  c.moveTo(startPosition.x, startPosition.y);
+    c.moveTo(startPosition.x, startPosition.y);
 
-  updatedPosition.x = getViewWidth(80);
-  drawLineToUpdatePosition();
+    updatedPosition.x = getViewWidth(80);
+    drawLineToUpdatePosition();
 
-  createRightTriangleFromCurrentPosition();
+    createRightTriangleFromCurrentPosition();
 
-  updatedPosition.x = getViewWidth(20);
-  drawLineToUpdatePosition();
+    updatedPosition.x = getViewWidth(20);
+    drawLineToUpdatePosition();
 
-  updatedPosition.x -= configs.length;
-  updatedPosition.y += configs.length;
-  drawLineToUpdatePosition();
+    updatedPosition.x -= configs.length;
+    updatedPosition.y += configs.length;
+    drawLineToUpdatePosition();
 
-  updatedPosition.x += configs.length;
-  updatedPosition.y += configs.length;
-  drawLineToUpdatePosition();
+    updatedPosition.x += configs.length;
+    updatedPosition.y += configs.length;
+    drawLineToUpdatePosition();
 
-  updatedPosition.x = getViewWidth(80);
-  drawLineToUpdatePosition();
+    updatedPosition.x = getViewWidth(80);
+    drawLineToUpdatePosition();
 
-  createRightTriangleFromCurrentPosition();
+    createRightTriangleFromCurrentPosition();
 
-  updatedPosition.x = 0;
-  drawLineToUpdatePosition();
+    updatedPosition.x = 0;
+    drawLineToUpdatePosition();
 
-  c.stroke();
+    c.stroke();
 }
 
 function drawLineToUpdatePosition() {
-  c.lineTo(updatedPosition.x, updatedPosition.y);
+    c.lineTo(updatedPosition.x, updatedPosition.y);
 }
 
 function updateResolution() {
-  canvas.width = window.innerWidth;
-  canvas.height = startPosition.y + configs.length * 6 + startPosition.y;
+    canvas.width = window.innerWidth;
+    canvas.height = startPosition.y + configs.length * 6 + startPosition.y;
 }
 
 function getViewHeight(height) {
-  return (height * window.innerHeight) / 100;
+    return (height * window.innerHeight) / 100;
 }
 
 function getViewWidth(width) {
-  return (width * window.innerWidth) / 100;
+    return (width * window.innerWidth) / 100;
 }
 
 function animateMyDrawing() {
-  if (alpha >= 0.85) {
-    flag = true;
-  }
-  if (alpha < 0.5) {
-    flag = false;
-  }
+    if (alpha >= 0.85) {
+        flag = true;
+    }
+    if (alpha < 0.5) {
+        flag = false;
+    }
 
-  if (flag) {
-    alpha -= 0.05;
-  } else {
-    alpha += 0.05;
-  }
-  drawShape(alpha);
+    if (flag) {
+        alpha -= 0.05;
+    } else {
+        alpha += 0.05;
+    }
+    drawShape(alpha);
 }
 
 function updateBoxesPosition() {
-  for (let i = 1; i <= 4; i++) {
-    let txtBox = document.querySelector(`#txt${i}`);
-    txtBox.style.top =
-      startPosition.y -
-      heightOfTxtBox / 2 +
-      2 * (i - 1) * configs.length -
-      6 +
-      "px";
-    if (i % 2 == 0) {
-      txtBox.style.right = getViewWidth(20) + 4 + "px";
-    } else {
-      txtBox.style.left = getViewWidth(20) + 4 + "px";
+    for (let i = 1; i <= 4; i++) {
+        let txtBox = document.querySelector(`#txt${i}`);
+        txtBox.style.top =
+            startPosition.y -
+            heightOfTxtBox / 2 +
+            2 * (i - 1) * configs.length -
+            6 +
+            "px";
+        if (i % 2 == 0) {
+            txtBox.style.right = getViewWidth(20) + 4 + "px";
+        } else {
+            txtBox.style.left = getViewWidth(20) + 4 + "px";
+        }
     }
-  }
 }
 
 function createRightTriangleFromCurrentPosition() {
-  updatedPosition.x += configs.length;
-  updatedPosition.y += configs.length;
-  drawLineToUpdatePosition();
+    updatedPosition.x += configs.length;
+    updatedPosition.y += configs.length;
+    drawLineToUpdatePosition();
 
-  updatedPosition.x -= configs.length;
-  updatedPosition.y += configs.length;
-  drawLineToUpdatePosition();
+    updatedPosition.x -= configs.length;
+    updatedPosition.y += configs.length;
+    drawLineToUpdatePosition();
 }
 
 function refresh() {
-  location.reload();
+    location.reload();
 }
