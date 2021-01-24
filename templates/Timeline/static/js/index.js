ea7fb0a1b548914f13e28b23ecad4a4a113a0161
Kamalpreet-3223 <ksingh7_be20@thapar.edu>
2021-01-25 00:34:46 +0530

    Modal added

--- templates/Timeline/static/js/index.js
+++ templates/Timeline/static/js/index.js
@@ -20,3 +20,48 @@ function showAgain() {
   day1.classList.remove("hidden");
   day2.classList.add("hidden");
 }
+
+$(".modal").each(function () {
+  $(this).wrap('<div class="overlay"></div>');
+});
+
+$(".open-modal").on("click", function (e) {
+  e.preventDefault();
+  e.stopImmediatePropagation;
+
+  var $this = $(this),
+    modal = $($this).data("modal");
+
+  $(modal).parents(".overlay").addClass("open");
+  setTimeout(function () {
+    $(modal).addClass("open");
+  }, 350);
+
+  $(document).on("click", function (e) {
+    var target = $(e.target);
+
+    if ($(target).hasClass("overlay")) {
+      $(target)
+        .find(".modal")
+        .each(function () {
+          $(this).removeClass("open");
+        });
+      setTimeout(function () {
+        $(target).removeClass("open");
+      }, 350);
+    }
+  });
+});
+
+$(".close-modal").on("click", function (e) {
+  e.preventDefault();
+  e.stopImmediatePropagation;
+
+  var $this = $(this),
+    modal = $($this).data("modal");
+
+  $(modal).removeClass("open");
+  setTimeout(function () {
+    $(modal).parents(".overlay").removeClass("open");
+  }, 350);
+});
