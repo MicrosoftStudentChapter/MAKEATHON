1e34ba5f49c5ac6f1219d89c3227f3c704abc9bf
Ritik <ritikrajdev761@gmail.com>
2021-02-11 18:02:12 +0530

    Whitenoise cache and compression support

--- Makeathon3077/settings/prod.py
+++ Makeathon3077/settings/prod.py
@@ -12,5 +12,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["makeathonmlsc.herokuapp.com"]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)  # noqa
