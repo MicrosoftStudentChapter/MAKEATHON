43cc1bfe7ca20c7e18cf52def5b279818f70e85d
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-02-14 00:34:25 +0530

    Added domain allowed host

--- Makeathon3077/settings/prod.py
+++ Makeathon3077/settings/prod.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["makeathonmlsc.herokuapp.com"]
+ALLOWED_HOSTS = ["makeathonmlsc.herokuapp.com", "makeathon3077.msctiet.co.in"]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
