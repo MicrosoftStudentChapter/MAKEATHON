35bb903463b2b6346836ea938fdf7acff4aa1a1d
ritikrajdev <ritikrajdev761@gmail.com>
2021-02-24 09:37:12 +0530

    Testing Url only available when Debug is on

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import index
+from django.conf import settings
 
-urlpatterns = [
-    path('testing/', index, name='index'),
-]
+urlpatterns = []
+
+if settings.DEBUG:
+    urlpatterns += [path('testing/', index, name='index-testing')]
+
