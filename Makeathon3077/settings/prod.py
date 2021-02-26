2e19978705eb0d3a4e1764b50b53c2b9a75c45bc
parthrr510 <42448056+parthrr510@users.noreply.github.com>
2021-02-26 20:12:28 +0530

    Update prod.py

--- Makeathon3077/settings/prod.py
+++ Makeathon3077/settings/prod.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["makeathonmlsc.herokuapp.com", "makeathon3077.msctiet.co.in"]
+ALLOWED_HOSTS = ["makeathonmlsc.herokuapp.com", "makeathon3077.msctiet.co.in","makeathon3077.mlsctiet.co.in"]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
