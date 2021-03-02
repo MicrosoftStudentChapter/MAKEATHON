d95b9ac0963f7b81f439ed5498e2fe0438da2273
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-03-02 22:18:40 +0530

    API endpoint for profile validation:

--- getyourbadge/views.py
+++ getyourbadge/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Profile
 
 
 def badge(request):
     return render(request, "getyourbadge/index.html")
+
+
+def ProfileDetails(request):
+    if request.method == "GET":
+        mail = request.headers["email"]
+        try:
+            profile = Profile.objects.get(email=mail)
+        except Exception:
+            return JsonResponse({
+                "error": "Invalid email-id",
+            })
+        profile_obj = {
+            "firstName": profile.firstName,
+            "lastName": profile.lastName,
+            "email": profile.email,
+            "teamName": profile.teamName
+        }
+        return JsonResponse(profile_obj)
+    return JsonResponse({"error": ":unamused: Not a GET request"})
