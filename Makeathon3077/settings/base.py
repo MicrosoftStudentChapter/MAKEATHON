e23af7e64508e02392f40e226145aadcb7bcf486
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-03-05 15:54:23 +0530

    Allow CORS headers

--- Makeathon3077/settings/base.py
+++ Makeathon3077/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -47,6 +48,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'email',
+]
+
 
 ROOT_URLCONF = "Makeathon3077.urls"
 
